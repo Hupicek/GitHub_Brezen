23550a8c794ccc9d74b85fa19aaa76038871311b
jan.ptacek <jan.ptacek@VT1-16>
2022-03-14 13:28:25 +0100

    doprovodny text

--- github/Program.cs
+++ github/Program.cs
@@ -1,8 +1,9 @@
-﻿Console.WriteLine("napis prvni cislo");
+﻿//napises cisla
+Console.WriteLine("napis prvni cislo");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("napis druhe cislo");
 int b = Convert.ToInt32(Console.ReadLine());
-
+//vypocita a vypise cisla
 int c = Math.Min(a, b);
 int d = Math.Max(a, b);
 Console.WriteLine(c);
