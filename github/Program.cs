4bf15743a69d8d1dbaac0d38bc4554537093ef89
jan.ptacek <jan.ptacek@VT1-16>
2022-03-14 12:05:27 +0100

    Přidejte soubory projektu.

---
+++ github/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("napis prvni cislo");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("napis druhe cislo");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = Math.Min(a, b);
+int d = Math.Max(a, b);
+Console.WriteLine(c);
+Console.WriteLine(d);
